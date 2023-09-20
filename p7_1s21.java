a616597f68b56e9f0c96648f71859eba503286fd
Angel Aldair Cruz Medina <144179088+Aldair1218@users.noreply.github.com>
2023-09-20 01:01:02 -0600

    Add files via upload

---
+++ p7_1s21.java
@@ -0,0 +1,10 @@
+class p7_1s21{
+	public static void main(String[] args){
+		byte edad = 19;
+		if (edad > 18){
+		System.out.nextLine("Ingresa tu edad" = 19);
+		System.out.nextInt("tu edad es" + edad);
+		System.out.println("Es mayor de edad");
+		}
+	}
+}
